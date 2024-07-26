55b8b6f9464c7cd5c71e25fa8f2e3d6976aed60e
youssef-223 <youssef.moustafa202@gmail.com>
2024-07-26 18:55:18 +0300

    Editin Script to run sudo command non-interactively

--- Scripts/mysql-install.sh
+++ Scripts/mysql-install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install mysql-server
+# Define the sudo password
+SUDO_PASSWORD='1234'
+
+# Function to run sudo command non-interactively
+run_sudo() {
+  echo $SUDO_PASSWORD | sudo -S $@
+}
+
+run_sudo apt-get update
+run_sudo apt-get install mysql-server
 
 # Function to start MySQL service
 start_mysql() {
   echo "Starting MySQL service..."
-  sudo systemctl start mysql
+  run_sudo systemctl start mysql
   if [ $? -eq 0 ]; then
     echo "MySQL service started successfully."
   else
@@ -18,7 +26,7 @@ start_mysql() {
 # Function to enable MySQL to start on boot
 enable_mysql_service() {
   echo "Enabling MySQL service to start on boot..."
-  sudo systemctl enable mysql
+  run_sudo systemctl enable mysql
   if [ $? -eq 0 ]; then
     echo "MySQL service enabled to start on boot successfully."
   else
