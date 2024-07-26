1d13a73e49d7b0ea592efe420a6dc182c6cd25db
youssef-223 <youssef.moustafa202@gmail.com>
2024-07-26 20:45:13 +0300

    solving the mysql login problem

--- Scripts/mysql-setup.sh
+++ Scripts/mysql-setup.sh
@@ -5,19 +5,14 @@ DB_HOST="jdbc:mysql://192.168.124.128:3306/"
 DB_NAME="giza_db"
 
 SUDO_PASSWORD=1234
-run_sudo() {
-  echo $SUDO_PASSWORD | sudo -S $@
-}
 
 # Create the database
 echo "Creating database ${DB_NAME}..."
-run_sudo mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+echo $SUDO_PASSWORD | sudo -S mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
 
 if [ $? -eq 0 ]; then
     echo ">> Database setup complete."
   else
     echo ">> Failed to create DB"
     exit 1
-  fi
-
-# echo "Database setup complete."
+fi
