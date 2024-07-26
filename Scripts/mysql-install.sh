267ca828c3f315b60ba3a1561f0fde75384f1ab9
youssef-223 <youssef.moustafa202@gmail.com>
2024-07-26 19:05:52 +0300

    beautifying the scripts output

--- Scripts/mysql-install.sh
+++ Scripts/mysql-install.sh
@@ -13,30 +13,30 @@ run_sudo apt-get install mysql-server
 
 # Function to start MySQL service
 start_mysql() {
-  echo "Starting MySQL service..."
+  echo ">> Starting MySQL service..."
   run_sudo systemctl start mysql
   if [ $? -eq 0 ]; then
-    echo "MySQL service started successfully."
+    echo ">> MySQL service started successfully."
   else
-    echo "Failed to start MySQL service."
+    echo ">> Failed to start MySQL service."
     exit 1
   fi
 }
 
 # Function to enable MySQL to start on boot
 enable_mysql_service() {
-  echo "Enabling MySQL service to start on boot..."
+  echo ">> Enabling MySQL service to start on boot..."
   run_sudo systemctl enable mysql
   if [ $? -eq 0 ]; then
-    echo "MySQL service enabled to start on boot successfully."
+    echo ">> MySQL service enabled to start on boot successfully."
   else
-    echo "Failed to enable MySQL service to start on boot."
+    echo ">> Failed to enable MySQL service to start on boot."
     exit 1
   fi
 }
 
 # Main script execution
-echo "Starting MySQL setup script..."
+echo ">>>Starting MySQL setup script..."
 
 # Start MySQL
 start_mysql
@@ -44,4 +44,4 @@ start_mysql
 # Enable MySQL service to start on boot
 enable_mysql_service
 
-echo "MySQL setup completed successfully."
+echo ">>> MySQL setup completed successfully."
